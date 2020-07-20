9ba39b78c9e0452cd543432da0de39539f7ca54e
Misikir <mikireyob@gmail.com>
2020-07-20 04:54:41 -0400

    move python tests to test dir

---
+++ test/test_names.py
@@ -0,0 +1,37 @@
+import requests
+import random
+
+names = [
+    "Boyd", "Jayde", "Brinley", "Boyd", "Blake", "Michael", "Boykin", "Alona",
+    "Mackenzie", "Boyle", "Mitchell", "Carson"
+    "Boyle", "Tyler", "Owen", "Brant", "Wayne", "Logan", "Brantwein", "Brooke",
+    "Chelsea", "Brashear", "Erin", "Isabella", "Braun", "Louise", "Sienna",
+    "Bravo", "Anthony", "Richard", "Breeden", "Noel", "Alahya"
+]
+
+zip_codes = [
+    "01234", "01269"
+    "04592", "02131", "03421", "01234", "92341", "02192", "91021", "34210",
+    "04321"
+]
+
+
+def main(num_of_requests=10):
+    for _ in range(num_of_requests):
+        first_name, middle_name, last_name = [
+            random.choice(names) for _ in range(3)
+        ]
+        zip_code = random.choice(zip_codes)
+        data = {
+            "first_name": first_name,
+            "middle_name": middle_name,
+            "last_name": last_name,
+            "zip_code": zip_code
+        }
+        r = requests.post(
+            'https://6ji66tn9s8.execute-api.us-east-1.amazonaws.com/test',
+            json=data)
+        print(r.status_code)
+
+
+main(100)
