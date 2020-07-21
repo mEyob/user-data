a2a225f0c167856751d4c35930ea791ab277e55c
Misikir <mikireyob@gmail.com>
2020-07-21 13:51:09 -0400

    create JSON validator and HTTP response maker

--- src/backend-lambda.py
+++ src/backend-lambda.py
@@ -2,8 +2,41 @@ import json
 import boto3
 
 
+def make_response(code, message):
+    """
+    Create a simple HTTP response.
+    """
+    return  {
+        'statusCode': code, 
+        'body': message
+        }
+
+def is_valid_json(json_data):
+    """Take a JSON data as an input and return true only
+    if it is correctly formed and of 'str' or 'dict' type.
+    """
+    if isinstance(json_data, str):
+        try: 
+            json.loads(json_data)
+        except:
+            return False
+    elif isinstance(json_data, dict):
+        try: 
+            json.dumps(json_data)
+        except:
+            return False
+    else:
+        return False
+    return True
+    
 def lambda_handler(event, context):
     request = event.get('body') or {}
+
+    if not request:
+        return make_response(400, "Empty request")
+    if not is_valid_json(request):
+        return make_response(400, "Malformed JSON")
+
     valid = [
         "first_name" in request, 
         "middle_name" in request, 
@@ -16,13 +49,6 @@ def lambda_handler(event, context):
         response = firehose_client.put_record(
             DeliveryStreamName="kinesis-firehose-extended-s3-stream",
             Record={'Data': json.dumps(request)})
-        return {
-            'statusCode': 200, 
-            'body': json.dumps(response)
-            }
-
+        return make_response(200, json.dumps(response))
     else:
-        return {
-            'statusCode': 400, 
-            'body': "BAD REQUEST"
-            }
+        return make_response(400, "JSON missing required keys")
